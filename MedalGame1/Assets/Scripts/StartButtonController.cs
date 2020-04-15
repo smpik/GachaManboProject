d2cb48d111c86a29cba3fa7a30ee450c5a12fe55
smpik <32724178+smpik@users.noreply.github.com>
2020-04-16 08:39:01 +0900

    TitleCanvas作成

---
+++ MedalGame1/Assets/Scripts/StartButtonController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButtonController : MonoBehaviour
+{
+	private GameObject TitleCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		TitleCanvas = GameObject.Find("TitleCanvas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	/* StartButtonがタップされた時の処理	*/
+	public void TapStartButton()
+	{
+		TitleCanvas.SetActive(false);//TitleCanvasを閉じる
+		//SE再生(やりたいなら。。。)
+	}
+}
