beef490735b4a4d004c584f51d4fa69da35fdd26
smpik <32724178+smpik@users.noreply.github.com>
2020-04-27 21:22:28 +0900

    V2.2.0_クレジット表示エリア拡大
    +連打防止機能強化

--- MedalGame1/Assets/Scripts/EnterCoin.cs
+++ MedalGame1/Assets/Scripts/EnterCoin.cs
@@ -7,36 +7,39 @@ public class EnterCoin : MonoBehaviour
 	private const int TIME_WAIT_COIN_ENTER = 5;//コイン連続投入を制限するための待ち時間
 
 	private CreditManager CreditManagerInstance;
+	private EnterCoinGateSensorController EnterCoinGateSensorControllerInstance;
 	private GameObject CoinPrefab;
 
-	private int WaitEnterCoinTimer;//コイン連続投入制限用待ち時間タイマー
+	//private int WaitEnterCoinTimer;//コイン連続投入制限用待ち時間タイマー
 
 
     // Start is called before the first frame update
     void Start()
     {
 		CreditManagerInstance = GameObject.Find("EnterCoinGate").GetComponent<CreditManager>();
+		EnterCoinGateSensorControllerInstance = GameObject.Find("EnterCoinGateSensor").GetComponent<EnterCoinGateSensorController>();
 		CoinPrefab = (GameObject)Resources.Load("Prefabs/Coin");
-		WaitEnterCoinTimer = 0;
+		//WaitEnterCoinTimer = 0;
 	}
 
-	void Update()
-	{
-		if(WaitEnterCoinTimer>0)//コイン連続投入防止用待ち時間があるなら
-		{
-			WaitEnterCoinTimer--;//カウントする
-		}
-	}
+	//void Update()
+	//{
+	//	if(WaitEnterCoinTimer>0)//コイン連続投入防止用待ち時間があるなら
+	//	{
+	//		WaitEnterCoinTimer--;//カウントする
+	//	}
+	//}
 
 	public void TapButtonCoinEnter()
 	{
-		if(WaitEnterCoinTimer<=0)//コイン連続投入防止用待ち時間がなしなら(コイン投入してよいなら)
+		//if( (WaitEnterCoinTimer<=0) && (EnterCoinGateSensorControllerInstance.IsCoinNothing()==true) )//コイン連続投入防止用待ち時間がなしなら(コイン投入してよいなら)
+		if (EnterCoinGateSensorControllerInstance.IsCoinNothing() == true)
 		{
 			if (CreditManagerInstance.IsCoinEnterPermited() == true)//Creditあるか確認
 			{   //あるなら
 				createCoin();//コイン生成
 				CreditManagerInstance.SubtractCredit();//クレジット--
-				WaitEnterCoinTimer = TIME_WAIT_COIN_ENTER;//コイン連続投入防止用待ち時間をセット
+				//WaitEnterCoinTimer = TIME_WAIT_COIN_ENTER;//コイン連続投入防止用待ち時間をセット
 			}
 		}
 	}
---
+++ MedalGame1/Assets/Scripts/EnterCoinGateSensorController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterCoinGateSensorController : MonoBehaviour
+{
+	private Vector3 PosStartRay;
+	private Vector3 PosEndRay;
+	private const float LengthRay = 0.304f;//Rayを飛ばす長さ
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		PosStartRay = GameObject.Find("EnterCoinGateSensor").transform.position;
+		PosEndRay = new Vector3(0,0,1);//z軸方向を向けばなんでもいい
+	}
+
+	public bool IsCoinNothing()
+	{
+		bool ret = true;//true=コインなし=コイン投入OK
+
+		/* Sensorの中心からz軸方向にLengthRayだけRayを飛ばし、衝突したオブジェクトの情報をhittedObjInfoに格納する	*/
+		RaycastHit hittedObjInfo;
+		if (Physics.Raycast(PosStartRay, PosEndRay, out hittedObjInfo, LengthRay))//Rayはオブジェクトの中心から出る
+		{
+			if (hittedObjInfo.collider.gameObject.tag == "tag_Coin")
+			{
+				ret = false;/* コインを検知したならコイン投入NG	*/
+			}
+		}
+
+		return ret;
+	}
+}
